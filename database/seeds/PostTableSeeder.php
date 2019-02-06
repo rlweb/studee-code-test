8986b776b1ff852f7cf63e4b42342854434a242f
Rhys Laval <rhyslaval@gmail.com>
2019-02-06 15:37:05 +0000

    Seed a post

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'author' => 'Rhys Laval',
+            'title' => 'First post!',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae bibendum purus. Aenean consectetur mattis diam, ac ornare diam congue quis. Nunc porta dolor auctor, vehicula mauris porttitor, dictum sapien. Phasellus lacinia ex nisi, vitae congue tortor pharetra et. In lorem dui, ullamcorper et aliquet at, vulputate varius massa. Morbi et neque odio. Aliquam id lacus vel metus rhoncus tincidunt.',
+        ]);
+    }
+}
